34900456e4c5e6c2916b2077fc1efc62bc6d7155
Axel Andersson <axean602@student.liu.se>
2022-12-12 18:57:12 +0100

    Final changes on the useeffect

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
+import { Typography } from "@mui/material";
+import { fontFamily, fontSize } from "@mui/system";
 import { useState, useEffect } from "react";
 import Button from "./components/Button";
 import ColorBox from "./components/ColorBox";
+
+
+const getRandomColor = () => {
+  let hexaList = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+  let randomcolor = '#';
+
+  for (let i = 0; i < 6; i++) {
+    let char = hexaList[Math.floor(Math.random() * hexaList.length)]
+    randomcolor = randomcolor + char
+  }
+  return randomcolor
+}
 
 function App() {
 
   const [color, setColor] = useState('')
   const [buttonColors, setButtonColors] = useState ([]);
-  const [correct, setCorrect] = useState(false)
+  const [result, setResult] = useState(undefined)
 
-
-  useEffect(() => {
+  const generateColors = () => {
     let correctColor = getRandomColor()
     setColor(correctColor)
     let colorList = shuffleColors([correctColor, getRandomColor(), getRandomColor()])
     setButtonColors(colorList)
+  }
+
+  useEffect(() => {
+    generateColors()
   }, [])
 
-  const getRandomColor = () => {
-    let hexaList = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
-    let randomcolor = '#';
 
-    for (let i = 0; i < 6; i++) {
-      let char = hexaList[Math.floor(Math.random() * hexaList.length)]
-      randomcolor = randomcolor + char
-    }
-    return randomcolor
-  }
 
   const shuffleColors = (list) => {
    let shuffledList = list.sort((a,b) => 0.5 - Math.random())
@@ -33,18 +41,27 @@ function App() {
   }
   const handleAnswerClick = (answer) => {
     if (answer === color) {
-      setCorrect(true)
-      console.log("Correct!")
+      setResult(false)
+      generateColors()
     } else {
-      setCorrect(false)
-      console.log("WRONG")
+      setResult(true)
     }
   }
 
   return (
     <div className="App">
       <column>
-
+      <div style={{justifyContent:'center', alignItems:'center'}}>{ 
+      result===true &&
+      <Typography align="center" sx={{fontSize:30, color:'red'}}>
+          Wrong, try again
+        </Typography>}
+      {result===false &&
+        <Typography align="center" sx={{fontSize:30, color:'green'}}>
+          Correct! What about this one?
+        </Typography>
+        }
+        </div>
       <ColorBox color={color} />
       {buttonColors.map(col => (
         <Button key={col} text={col} onClick={() => handleAnswerClick(col)} /> 
--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ColorBox = ( { color }) => { 
   return (
-    <div className='color-box' style={{background: `${color}`}} >
+    <div className='color-box' style={{background: `${color}`, marginTop: 80}} >
     </div> 
   )
 }
